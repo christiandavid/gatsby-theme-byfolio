a26258a7ed4cda437b892a164c3e8aa67d3930a7
Christian David <christianibarguen@gmail.com>
2019-11-04 18:17:12 -0500

    Add LayoutContactMe component, add CV in PDF Format

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import { css } from "@emotion/core"
 import { graphql } from "gatsby"
 import AniLink from "gatsby-plugin-transition-link/AniLink"
 import { rhythm } from "../utils/typography"
-import Layout from "../components/layout"
+import LayoutContactMe from "../components/layout/layout-contact-me"
 
 export default ({ data }) => {
   return (
@@ -15,7 +15,7 @@ export default ({ data }) => {
         <link rel="canonical" href="http://mysite.com/example" />
         <body className="no-js" />
       </Helmet>
-      <Layout title={`Skills`}>
+      <LayoutContactMe title={`Skills`}>
         <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
         {data.allMarkdownRemark.edges.map(({ node }) => (
           <div key={node.id}>
@@ -38,7 +38,7 @@ export default ({ data }) => {
             </AniLink>
           </div>
         ))}
-      </Layout>
+      </LayoutContactMe>
     </>
   )
 }
