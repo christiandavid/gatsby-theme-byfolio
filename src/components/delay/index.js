637f95fa05a97a6da4f3913fe4779b658a9c4828
Christian David <christianibarguen@gmail.com>
2019-11-07 14:40:49 -0500

    Add Delay, Skill and AnimateItem component, add GraphQL Query for Skill page

---
+++ src/components/delay/index.js
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from "react"
+import PropTypes from "prop-types"
+import { jsx } from "@emotion/core"
+import styles from "./delay.css"
+
+/**
+ * Delay the component load for the specified time
+ *
+ * @param {integer} children
+ * @param {integer} cb callback funtion
+ * @param {integer} showLoadingAnimation show loading animation
+ * @param {integer} wait time to wait in seconds
+ */
+const Delay = ({
+  children,
+  cb = () => {},
+  showLoadingAnimation = true,
+  wait = 600,
+}) => {
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false)
+      cb()
+    }, wait)
+
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [cb, wait])
+
+  return !loading ? (
+    children
+  ) : showLoadingAnimation ? (
+    <div css={styles.delayLoading}></div>
+  ) : (
+    <></>
+  )
+}
+
+Delay.propTypes = {
+  children: PropTypes.node.isRequired,
+  cb: PropTypes.func,
+  showLoadingAnimation: PropTypes.bool,
+  wait: PropTypes.number,
+}
+
+export default Delay
