351be47a22408a43d1db18a47d6e8f690c81c90f
Christian David <christianibarguen@gmail.com>
2019-11-12 18:15:35 -0500

    Change the typography, add configuration for Netlify, add configuration for Sitemaps, Google analytics and add the portfolio description in the configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,9 +5,9 @@
  */
 
 module.exports = {
-  /* Your site config here */
   siteMetadata: {
     title: `Portfolio`,
+    siteUrl: `https://www.christianibarguen.com`,
   },
   plugins: [
     {
@@ -28,13 +28,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `GatsbyJS`,
-        short_name: `GatsbyJS`,
+        name: `Christian David Ibarguen`,
+        short_name: `CD`,
+        description: `This cool App contains information about my work experience as a software developer.`,
+        lang: `en`,
         start_url: `/`,
-        background_color: `#6b37bf`,
-        theme_color: `#6b37bf`,
-        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
-        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        background_color: `#000`,
+        theme_color: `#fff`,
         display: `standalone`,
         icon: `src/images/icon.png`, // This path is relative to the root of the site.
       },
@@ -47,7 +47,6 @@ module.exports = {
     },
     `gatsby-plugin-offline`,
     `gatsby-plugin-react-helmet`,
-    // `gatsby-plugin-transition-link`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -62,5 +61,43 @@ module.exports = {
       },
     },
     `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        trackingIds: [
+          "UA-151335375-1", // Google Analytics / GA
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          anonymize_ip: true,
+          cookie_expires: 0,
+          send_page_view: true,
+          cookie_name: "christianibarguen.com",
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Setting this parameter is also optional
+          respectDNT: false,
+        },
+      },
+    },
+    {
+      resolve: `gatsby-plugin-nprogress`,
+      options: {
+        // Setting a color is optional.
+        // color: `tomato`,
+        // Disable the loading spinner.
+        showSpinner: false,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-sitemap`,
+      options: {
+        exclude: [`/experience/_additionalSkills`],
+      },
+    },
   ],
 }
