1d62488cf1427a14af556506833626edf6d0edef
Christian David <christianibarguen@gmail.com>
2019-11-11 20:23:11 -0500

    Add Experience component

--- src/components/experience/index.js
+++ src/components/experience/index.js
@@ -3,7 +3,9 @@ import PropTypes from "prop-types"
 import AniLink from "gatsby-plugin-transition-link/AniLink"
 import Img from "gatsby-image"
 import { jsx } from "@emotion/core"
+import useMQResize from "../hooks/useMQResize"
 import Delay from "../delay"
+import isSSR from "../../utils/isSSR"
 import styles from "./experience.css"
 
 const mediaQueries = {
@@ -21,7 +23,50 @@ const Experience = ({
 }) => {
   const [loaded, setLoaded] = useState(false)
   const gridRef = useRef(null)
+  const matchPoints = useMQResize(mediaQueries)
   const jobItems = useRef(jobs.map(createRef))
+
+  if (loaded && !isSSR) {
+    // It's mandatory to specify the height when using flex column
+    const defaultMinMargin = 9999
+    let itemsHeight = 0
+    let maxMarginTop = 0
+    let minMarginTop = defaultMinMargin
+
+    for (let item of jobItems.current) {
+      // Get margin top and Bottom size to add it to the height later
+      const styles = window.getComputedStyle(item.current)
+      const marginTop = parseFloat(styles["marginTop"], 10)
+
+      if (marginTop) {
+        if (marginTop > maxMarginTop) {
+          maxMarginTop = marginTop
+        }
+        if (marginTop < minMarginTop) {
+          minMarginTop = marginTop
+        }
+      }
+
+      itemsHeight +=
+        item.current.offsetHeight + parseFloat(styles["marginBottom"], 10)
+    }
+    minMarginTop = minMarginTop === defaultMinMargin ? 1 : minMarginTop
+    if (itemsHeight) {
+      // Media queries specified in CSS; The size change by the amount of columns,
+      // each column and media query has different margin top and bottom
+      if (!matchPoints.max36 && !matchPoints.min52) {
+        itemsHeight += jobs.length * minMarginTop
+        itemsHeight /= 2
+      } else if (matchPoints.min52) {
+        itemsHeight += jobs.length * minMarginTop
+        itemsHeight /= 3
+        itemsHeight += maxMarginTop
+      } else {
+        itemsHeight += jobs.length * minMarginTop + maxMarginTop
+      }
+      gridRef.current.style.height = `${Math.ceil(itemsHeight)}px`
+    }
+  }
 
   function handleLoad() {
     setLoaded(true)
---
+++ src/utils/isSSR.js
@@ -0,0 +1,3 @@
+const isSSR = typeof window === "undefined"
+
+export default isSSR
